c8ed5dffb0cabc401d8f8c7ad79020ed520440c2
Alice <antgafl@gmail.com>
2019-07-28 17:31:18 -0400

    Scraper now works indefinitely

--- src/main/scala/wtf/shekels/alice/proxyscraper/impl/RunescapeScraper.scala
+++ src/main/scala/wtf/shekels/alice/proxyscraper/impl/RunescapeScraper.scala
@@ -10,10 +10,21 @@ import wtf.shekels.alice.proxyscraper.requests.ProxyScraper
 class RunescapeScraper {
 
   val scrape: StateT[IO, RequestState, Option[Json]] = StateT { s =>
-    ProxyScraper.request(makeUri(s), s.proxiedClients.head).map { r =>
-      println(r)
-      r
-    }.tupleLeft(RequestState(s.proxiedClients.tail :+ s.proxiedClients.head, s.index + 1))
+    ProxyScraper.request(makeUri(s), s.proxiedClients.head).attemptT.semiflatMap { r =>
+      IO {
+        println(r)
+        r
+      }.tupleLeft(RequestState(s.proxiedClients.tail :+ s.proxiedClients.head, s.index + 1))
+    }.leftSemiflatMap { e =>
+      IO {
+//        e.printStackTrace()
+        None
+      }.tupleLeft(RequestState(s.proxiedClients.tail, s.index + 1))
+    }.merge
+//    ProxyScraper.request(makeUri(s), s.proxiedClients.head).map { r =>
+//      println(r)
+//      r
+//    }.tupleLeft(RequestState(s.proxiedClients.tail :+ s.proxiedClients.head, s.index + 1))
   }
 
   def makeUri(requestState: RequestState): Uri = {
