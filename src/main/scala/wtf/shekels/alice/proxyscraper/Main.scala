f3244ce4f3c79bd69d7bf127cd1eb258b4d49bdc
Alice <antgafl@gmail.com>
2019-07-28 16:25:56 -0400

    It's FP now

--- src/main/scala/wtf/shekels/alice/proxyscraper/Main.scala
+++ src/main/scala/wtf/shekels/alice/proxyscraper/Main.scala
@@ -1,25 +1,25 @@
 package wtf.shekels.alice.proxyscraper
 
 import cats.effect._
-import cats.syntax.all._
 import org.apache.log4j.BasicConfigurator
 import wtf.shekels.alice.proxyscraper.impl.{RequestState, RunescapeScraper}
 import wtf.shekels.alice.proxyscraper.requests.ProxyScraper
 
 object Main extends IOApp {
+
+  val scraper = new RunescapeScraper
+
   def run(args: List[String]): IO[ExitCode] = {
     BasicConfigurator.configure()
 
-    ProxyScraper.getProxies.map {
+    ProxyScraper.getProxies.flatMap {
       case Some(proxies) =>
         println(proxies.size)
-        scrape(RequestState(proxies, 2))
-      case None =>
-    }.as(ExitCode.Success)
+        doItForever(RequestState(proxies, 2))(scraper.scrape.runS)
+    }
   }
 
-  def scrape(rs: RequestState): Unit = {
-    val scraper = new RunescapeScraper
-    scrape(scraper.scrape.run(rs).unsafeRunSync()._1)
+  def doItForever(start: RequestState)(loop: RequestState => IO[RequestState]): IO[Nothing] = {
+    loop(start).flatMap(doItForever(_)(loop))
   }
 }
